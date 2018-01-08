00257bd3637938745a36940ffefa56d84a5cd1e5
Robert L Carpenter <robert@robacarp.com>
2018-01-08 12:35:47 -0700

    use RelativeTime in ping_result.cr

--- src/models/ping_result.cr
+++ src/models/ping_result.cr
@@ -3,7 +3,7 @@ require "granite_orm/adapter/pg"
 class PingResult < Granite::ORM::Base
   adapter pg
 
-  belongs_to :check
+  belongs_to :ip_address
 
   field is_up : Bool
   field response_time : Float32
@@ -13,27 +13,7 @@ class PingResult < Granite::ORM::Base
 
   table_name "ping_results"
 
-  def relative_time_since : String
-    unless timestamp = created_at
-      return "never"
-    end
-
-    now = Time.now.to_utc
-    delta = now - timestamp
-
-    case
-    when delta < 10.seconds
-      "just now"
-    when delta < 59.seconds
-      "< 1m"
-    when delta < 59.minutes
-      "#{delta.minutes}m"
-    when delta < 24.hours
-      "#{delta.hours}h"
-    else
-      "too long ago"
-    end
-  end
+  include RelativeTime
 
   def is_up?
     is_up
