8da1965c20e5ac71cfb2a3995a739391607327ca
Robert L Carpenter <robert@robacarp.com>
2018-01-26 19:00:57 -0700

    Allow ip addresses to be forgotten

---
+++ src/controllers/ip_address_controller.cr
@@ -0,0 +1,42 @@
+class IpAddressController < ApplicationController
+  authorize_with IpAddressPolicy, IpAddress
+  require_logged_in
+
+  def delete
+    if ip_address = IpAddress.find params["id"]
+      authorize ip_address
+      domain = ip_address.domain.not_nil!
+
+      render "delete.slang"
+    else
+      flash["warning"] = "IP address doesnt exist."
+      redirect_to_domains
+    end
+  end
+
+  def destroy
+    unless ip_address = IpAddress.find params["id"]
+      flash["warning"] = "IP address doesnt exist."
+      redirect_to_domains
+      return
+    end
+
+    domain = ip_address.domain.not_nil!
+
+    unless params["confirm"] == "1"
+      flash["info"] = "You must check the confirm box"
+      skip_authorization
+      return render "delete.slang"
+    end
+
+    authorize ip_address
+
+    if ip_address.destroy
+      flash["info"] = "IP address forgotten."
+      redirect_to_domains
+    else
+      flash["error"] = "Unable to forget IP address."
+      redirect_to_domains
+    end
+  end
+end
