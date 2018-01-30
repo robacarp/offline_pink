71adf8cb78378135d694c643108d177b850fb4b7
Robert L Carpenter <robert@robacarp.com>
2018-01-29 22:12:07 -0700

    Switch from mosquito#puts to #log

--- src/jobs/ping_job.cr
+++ src/jobs/ping_job.cr
@@ -18,19 +18,19 @@ class PingJob < Mosquito::QueuedJob
 
   def perform
     ensure_domain_exists
-    print "Pinging: Domain##{domain.id} #{domain.name}"
+    log "Pinging: Domain##{domain.id} #{domain.name}"
     resolve_hosts && send_pings
 
     case @status
     when :success
-      print "Ping successful. #{@ip_addresses.size} addresses found."
+      log "Ping successful. #{@ip_addresses.size} addresses found."
     when :no_name_present
-      print "Invalid domain"
+      log "Invalid domain"
     when :no_host_present
-      print "Could not determine Hostname"
       ping_result is_up: false
+      log "Could not determine Hostname"
     else
-      print "Non translatable status: #{@status}"
+      log "Non translatable status: #{@status}"
     end
 
   rescue e : Socket::Error
@@ -48,7 +48,10 @@ class PingJob < Mosquito::QueuedJob
     # Resolve a list of host addresses for the domain
     hosts = Socket::Addrinfo.resolve(name, "http", type: Socket::Type::STREAM, protocol: Socket::Protocol::TCP).map(&.ip_address)
 
+    log "Found #{hosts.size} hosts"
+
     hosts.each do |host|
+      log "found #{host}, saving"
       # Search for each host address in the database
       existing_address_index = saved_addresses.index do |ip_address|
         host.address == ip_address.address
@@ -61,7 +64,7 @@ class PingJob < Mosquito::QueuedJob
         address = IpAddress.new domain_id: domain.id, address: host.address, version: "ipv4"
 
         if host.address.includes? ":"
-          print "not adding ipv6 address to database: #{host.address}"
+          log "not adding ipv6 address to database: #{host.address}"
           next
         end
 
@@ -76,13 +79,13 @@ class PingJob < Mosquito::QueuedJob
   def send_pings
     results = @ip_addresses.map do |a|
       if a.v6?
-        print "Skipping ipv6 address #{a.address}"
+        log "Skipping ipv6 address #{a.address}"
         next
       end
 
       next unless address = a.address
 
-      print "Pinging #{address}"
+      log "Pinging #{address}"
 
       statistics = ICMP::Ping.ping(address)
 
