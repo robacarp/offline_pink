df700325b0ac1ee2dd246bfe5ec94454eb23cdba
Robert L Carpenter <robert@robacarp.com>
2018-01-31 14:57:41 -0700

    Some summary info for myself, the admin

--- src/controllers/admin/home_controller.cr
+++ src/controllers/admin/home_controller.cr
@@ -1,6 +1,8 @@
 class Admin::HomeController < Admin::Controller
   def show
     user_count = User.where(email: "robert@robacarp.com").count
+    domain_count = Domain.count
+    route_count = Route.count
     render "show.slang"
   end
 end
