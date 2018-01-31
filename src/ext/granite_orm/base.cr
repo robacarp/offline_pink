97fcf5ab8bd5cd7335ba4f6d5c7dfc59b5947223
Robert L Carpenter <robert@robacarp.com>
2018-01-31 11:52:01 -0700

    Proper generics mean this hack isn't needed

--- src/ext/granite_orm/base.cr
+++ src/ext/granite_orm/base.cr
@@ -1,36 +1,6 @@
 class Granite::ORM::Base
   include Pink::Validator
 
-  # Hack to get crystal compiler to recognize that these methods will
-  # be here eventually, and will always return a string.
-  #
-  # These methods must be defined in finished, because they must be
-  # undefined when macros run. The eventual existence of the method
-  # is overshadowed by the macro definition of the same name so they
-  # cannot be defined before the end of the macro run.
-  #
-  # Additionally, it doesn't seem like crystal supports abstract class
-  # methods.
-  macro finished
-    def self.table_name
-      ""
-    end
-
-    def self.primary_name
-      ""
-    end
-
-    def self.adapter : Nil | Granite::Adapter::Base
-    end
-
-    def self.fields : Array(String)
-      [] of String
-    end
-
-    def self.from_sql(result)
-    end
-  end
-
   macro __field_bang
     {% for name, type in FIELDS %}
       property! {{name.id}} : Union({{type.id}} | Nil)
