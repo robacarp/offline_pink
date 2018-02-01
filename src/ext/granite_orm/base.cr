c3164b3abb51ea063a6e9edef3d1cc136ce395fa
Robert L Carpenter <robert@robacarp.com>
2018-02-01 11:40:52 -0700

    Make validations actually work again
    I don't understand why this didn't work.

--- src/ext/granite_orm/base.cr
+++ src/ext/granite_orm/base.cr
@@ -1,5 +1,24 @@
 class Granite::ORM::Base
-  include Pink::Validator
+  def validate : Nil
+  end
+
+  def valid? : Bool
+    clean_errors
+    validate
+    errors.none?
+  end
+
+  def clean_errors
+    errors = [] of Error
+  end
+
+  def add_error(field : Symbol, message : String)
+    errors << Error.new(field, message)
+  end
+
+  def new_record?
+    @id.nil?
+  end
 
   macro __field_bang
     {% for name, type in FIELDS %}
