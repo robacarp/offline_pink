955673ea2dd23eb998f69263164ed0de16c16fbd
Robert L Carpenter <robert@robacarp.com>
2018-12-15 22:20:21 -0700

    disabling password resets for now

--- src/actions/password_resets/edit.cr
+++ src/actions/password_resets/edit.cr
@@ -1,8 +1,8 @@
-class PasswordResets::Edit < BrowserAction
-  include Auth::PasswordResets::Base
-  include Auth::PasswordResets::TokenFromSession
-
-  get "/password_resets/:user_id/edit" do
-    render NewPage, form: PasswordResetForm.new, user_id: user_id.to_i
-  end
-end
+# class PasswordResets::Edit < BrowserAction
+#   include Auth::PasswordResets::Base
+#   include Auth::PasswordResets::TokenFromSession
+# 
+#   get "/password_resets/:user_id/edit" do
+#     render NewPage, form: PasswordResetForm.new, user_id: user_id.to_i
+#   end
+# end
