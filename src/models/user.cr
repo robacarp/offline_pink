de1ba1a7a5fde2313e405ab79e077fdd308e6c8e
Robert L Carpenter <robert@robacarp.com>
2018-12-21 10:48:40 -0700

    break out user password methods

--- src/models/user.cr
+++ src/models/user.cr
@@ -1,6 +1,9 @@
+require "./user/*"
+
 class User < BaseModel
   include Carbon::Emailable
 
+  include Password
   table :users do
     column email : String
     column crypted_password : String
@@ -15,13 +18,5 @@ class User < BaseModel
     Carbon::Address.new(email)
   end
 
-  def self.hash_password(unencrypted_password : String)
-    Crypto::Bcrypt::Password.create(unencrypted_password, cost: 10).to_s
-  end
-
-  def valid_password?(password_guess : String)
-    if hash = @crypted_password
-      Crypto::Bcrypt::Password.new(hash) == password_guess
-    end
   end
 end
---
+++ src/models/user/password.cr
@@ -0,0 +1,17 @@
+module Password
+  macro included
+    extend ClassMethods
+  end
+
+  module ClassMethods
+    def hash_password(unencrypted_password : String)
+      Crypto::Bcrypt::Password.create(unencrypted_password, cost: 10).to_s
+    end
+  end
+
+  def valid_password?(password_guess : String)
+    if hash = @crypted_password
+      Crypto::Bcrypt::Password.new(hash) == password_guess
+    end
+  end
+end
