b0be1f7d03e670cd21a0986a611d45e768818571
Robert L Carpenter <robert@robacarp.com>
2018-12-20 22:14:16 -0700

    simplified html pages

--- src/pages/home_page.cr
+++ src/pages/home_page.cr
@@ -2,9 +2,7 @@ class HomePage < GuestLayout
   def content
     div class: "row" do
       div class: "col-md-6 offset-md-3" do
-        para do
-          text "Offline pink monitors domain and service availability and connectivity with a few simple principals:"
-        end
+        para "Offline pink monitors domain and service availability and connectivity with a few simple principals:"
 
         ul do
           li do
@@ -27,23 +25,23 @@ class HomePage < GuestLayout
 
         h4 "Notifications"
 
-        para do
-          text "Service interruption notifications aren't being sent yet. This feature is under development."
-        end
+        para <<-TEXT
+          Service interruption notifications aren't being sent yet. This feature is under development.
+        TEXT
 
         hr
 
         h4 "Membership"
 
-        para do
-          text "Offline.pink is in private alpha testing. Membershipara is freely available but the ability to"
-          text "monitor domains and services is limited to invited users."
-        end
+        para <<-TEXT
+          Offline.pink is in private alpha testing. Membershipara is freely available but the ability to
+          monitor domains and services is limited to invited users.
+        TEXT
 
-        para do
-          text "Invitations are available for a small number of users willing to provide feedback during the"
-          text "testing phases."
-        end
+        para <<-TEXT
+          Invitations are available for a small number of users willing to provide feedback during the
+          testing phases.
+        TEXT
 
         hr
 
@@ -55,9 +53,7 @@ class HomePage < GuestLayout
               em "2018-06-06"
             end
 
-            para do
-              text "User profiles are now editable."
-            end
+            para "User profiles are now editable."
           end
 
           li do
@@ -65,9 +61,7 @@ class HomePage < GuestLayout
               em "2018-04-26"
             end
 
-            para do
-              text "Added invite functionality. New users must be invited or activated by an Admin."
-            end
+            para "Added invite functionality. New users must be invited or activated by an Admin."
           end
 
           li do
@@ -75,9 +69,7 @@ class HomePage < GuestLayout
               em "2018-02-07"
             end
 
-            para do
-              text "Offline.pink is now minimally configured to send emails."
-            end
+            para "Offline.pink is now minimally configured to send emails."
           end
 
 
@@ -86,9 +78,7 @@ class HomePage < GuestLayout
               em "2018-01-17"
             end
 
-            para do
-              text "Monitoring begins"
-            end
+            para "Monitoring begins"
           end
         end
 
