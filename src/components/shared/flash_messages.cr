6162052195f9f0ef198df714b8414728dd7a8174
Robert L Carpenter <robert@robacarp.com>
2018-12-21 23:59:13 -0700

    fix broken flash message names for failure

--- src/components/shared/flash_messages.cr
+++ src/components/shared/flash_messages.cr
@@ -3,7 +3,7 @@ module Shared::FlashMessages
     div class: "container" do
       div class: "row" do
         @context.flash.each do |flash_type, flash_message|
-          div class: "alert alert-#{flash_type}", flow_id: "flash" do
+          div class: "alert alert-#{decode_flash flash_type}", flow_id: "flash" do
             para do
               text flash_message
             end
@@ -13,6 +13,15 @@ module Shared::FlashMessages
 
       div class: "row" do
       end
+    end
+  end
+
+  def decode_flash(message)
+    case message
+    when "failure"
+      "danger"
+    else
+      message
     end
   end
 end
