ecdf004cd0c2ebeaf2716330142480b24feb80db
Rob <robert@robacarp.com>
2020-11-13 16:07:35 -0700

    checkbox helper and css

---
+++ src/components/shared/checkbox.cr
@@ -0,0 +1,24 @@
+class Shared::Checkbox(T) < BaseComponent
+  # Raises a helpful error if component receives an unpermitted attribute
+  include Lucky::CatchUnpermittedAttribute
+
+  needs attribute : Avram::PermittedAttribute(T)
+  needs label_text : String?
+
+  def render
+    classes = ["field checkbox"]
+    unless attribute.valid?
+      classes << "error"
+    end
+
+    div class: classes.join(' ') do
+      with_defaults field: attribute do |input_builder|
+        yield input_builder
+      end
+
+      label_for attribute, label_text
+
+      mount Shared::FieldErrors, attribute
+    end
+  end
+end
