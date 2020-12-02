3c56d7f24c7f334b8bacc2a8fc05693301bd93b7
Rob <robert@robacarp.com>
2020-12-02 14:05:32 -0700

    restyle domains#new page

--- src/pages/domains/new.cr
+++ src/pages/domains/new.cr
@@ -2,15 +2,19 @@ class Domains::NewPage < AuthLayout
   needs save : SaveDomain
 
   def content
-    shrink_to_fit do
-      form_for Domains::Create do
-        h1 "Monitor new domain"
-        hr
+    small_frame do
+      h1 "Monitor new domain"
+      hr
 
-        mount Shared::OwnershipDropdown, attribute: save.owner, possible_owners: possible_owners
-        mount Shared::Field, attribute: save.name, label_text: "DNS Name", &.text_input(autofocus: true, placeholder: "google.com")
+      shrink_to_fit do
+        form_for Domains::Create do
+          mount Shared::OwnershipDropdown, attribute: save.owner, possible_owners: possible_owners
+          mount Shared::Field, attribute: save.name, label_text: "DNS Name", &.text_input(autofocus: true, placeholder: "google.com")
 
-        submit "Submit"
+          shrink_to_fit do
+            submit "Submit"
+          end
+        end
       end
     end
   end
