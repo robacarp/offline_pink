4699046f5c50e79c4c94a34008c4ecbfa217c607
Rob <robert@robacarp.com>
2020-12-16 11:13:13 -0700

    monitor # show pages

--- src/pages/monitor/http/show_page.cr
+++ src/pages/monitor/http/show_page.cr
@@ -2,6 +2,14 @@ class Monitor::Http::ShowPage < AuthLayout
   needs monitor : Monitor::Http
 
   def content
-    text "yolo http"
+    small_frame do
+      header_and_links do
+        h1 "http monitor"
+
+        div do
+          a "delete", href: "#"
+        end
+      end
+    end
   end
 end
--- src/pages/monitor/icmp/show_page.cr
+++ src/pages/monitor/icmp/show_page.cr
@@ -2,6 +2,14 @@ class Monitor::Icmp::ShowPage < AuthLayout
   needs monitor : Monitor::Icmp
 
   def content
-    text "yolo icmp"
+    small_frame do
+      header_and_links do
+        h1 "icmp monitor"
+
+        div do
+          a "delete", href: "#"
+        end
+      end
+    end
   end
 end
