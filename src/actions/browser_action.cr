6e6bbc6e55d9b41f0d61586d2c808c0b8585044d
Rob <robert@robacarp.com>
2021-01-22 15:27:43 -0700

    only lookup current user once

--- src/actions/browser_action.cr
+++ src/actions/browser_action.cr
@@ -11,8 +11,12 @@ abstract class BrowserAction < Lucky::Action
   expose current_user
 
   # This method tells Authentic how to find the current user
+  @users : Hash(String, User?)?
   private def find_current_user(id) : User?
-    UserQuery.new.id(id).first?
+    @users ||= Hash(String, User?).new do |hash, key|
+      hash[key] = UserQuery.new.id(key).first?
+    end
+    @users.try { |users| users[id] }
   end
 
 end
