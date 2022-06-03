d0e81a6428f922ac9fd47f7786e441ed104b6cb6
rob <robacarp@users.noreply.github.com>
2022-06-02 20:54:08 -0600

    validate connection to the database on a fly.io health check

--- src/actions/home/ping.cr
+++ src/actions/home/ping.cr
@@ -2,8 +2,12 @@ class Home::Ping < BrowserAction
   allow_guests
 
   get "/ping" do
+    # validate that the database is still connected
+    region = RegionQuery.first
+
     response = Hash(String, String).new
     response["ok"] = "true"
+    response["region"] = region.name
 
     response.merge! Build.details
 
