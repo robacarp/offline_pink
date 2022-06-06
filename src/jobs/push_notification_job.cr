428ab53d8eef12c188bccac9c3ee5d3b85ef7c54
rob <robacarp@users.noreply.github.com>
2022-06-06 13:55:49 -0600

    switch off push notifications for jobs when user notifications aren't allowed

--- src/jobs/push_notification_job.cr
+++ src/jobs/push_notification_job.cr
@@ -6,6 +6,7 @@ class PushNotificationJob < Mosquito::QueuedJob
   )
 
   def perform
+    return unless push_enabled?
     user = UserQuery.new.find user_id
     return unless pushover_key = user.pushover_key
 
@@ -14,5 +15,14 @@ class PushNotificationJob < Mosquito::QueuedJob
       title: title,
       message: notification
     )
+  end
+
+  def push_enabled?
+    unless Featurette[:push_notifications].enabled?(for: user_id)
+      log "refusing to send notification because user #{user_id} is not enabled for push notification"
+      return false
+    end
+
+    true
   end
 end
