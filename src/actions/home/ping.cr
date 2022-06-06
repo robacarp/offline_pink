49342dfd3024ed12268b8c90229b7b35fae00eab
rob <robacarp@users.noreply.github.com>
2022-06-06 13:24:12 -0600

    disables query cache for region lookup in ping.json endpoint

--- src/actions/home/ping.cr
+++ src/actions/home/ping.cr
@@ -2,8 +2,17 @@ class Home::Ping < BrowserAction
   allow_guests
 
   get "/ping" do
+    # validate that the database is still connected
+    region = nil
+
+    Avram.temp_config(query_cache_enabled: false) do
+      region = RegionQuery.first
+    end
+
+
     response = Hash(String, String).new
     response["ok"] = "true"
+    response["region"] = region.name
 
     response.merge! Build.details
 
