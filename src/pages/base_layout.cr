50c314b7f8eddb6dd4d8dc7443cf5bf275da8a36
rob <robacarp@users.noreply.github.com>
2022-10-14 11:14:01 -0600

    fixes sudo toolbar

--- src/pages/base_layout.cr
+++ src/pages/base_layout.cr
@@ -46,24 +46,18 @@ abstract class BaseLayout
   def sudo_toolbar
     return unless sudo_viewer = current_user
 
-    div id: "sudo-toolbar" do
+    mount Shared::AlertBox, severity: "info" do
       para do
-        text "Impersonating "
+        text "Impersonating user#"
+        text sudo_viewer.id
+        text "("
         text sudo_viewer.email
+        text ")"
       end
 
-      link to: Admin::Users::EndImpersonation, data_confirm: "Are you sure?" do
-        span class: "close" do
-          tag "svg", role: "button", xmlns: "http://www.w3.org/2000/svg", viewBox:"0 0 20 20" do
-            tag "title", "Close"
-            tag "path",
-              d: <<-SVG
-                M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z
-              SVG
-          end
-        end
+      para do
+        link "Return to safety", to: Admin::Users::EndImpersonation, data_confirm: "Are you sure?"
       end
-
     end
   end
 
