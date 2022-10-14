6bed3b7fa49887d68aa51ab1bdfb09b45fef784a
rob <robacarp@users.noreply.github.com>
2022-10-14 15:39:24 -0600

    rewrite http monitor to manually manage openssl
    openssl failures were due to not correctly coding the dns_name into the connection. previously the IP address of the targeted host would show up.

--- src/jobs/monitoring/http_monitor.cr
+++ src/jobs/monitoring/http_monitor.cr
@@ -9,7 +9,72 @@ module Monitoring
       @domain ||= monitor.domain!
     end
 
-    def config
+    def port
+      # TODO allow manually specifying the port
+      if config.ssl?
+        443
+      else
+        80
+      end
+    end
+
+    def headers : HTTP::Headers
+      HTTP::Headers{
+        "Host" => domain.name
+        # TODO "Accept-Encoding" => "gzip, deflate"
+      }
+    end
+
+    def tls_config
+      OpenSSL::SSL::Context::Client.new.tap do |context|
+        context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
+      end
+    end
+
+    # TODO what are sane values for read_timeout, write_timeout, connect_timeout
+    def with_connected_io
+      tcp_socket = TCPSocket.new host.ip_address, port#, connect_timeout: 5
+      # tcp_socket.write_timeout = 2
+      # tcp_socket.read_timeout = 2
+      tcp_socket.sync = false
+
+      if config.ssl?
+        ssl_layer = OpenSSL::SSL::Socket::Client.new(
+          tcp_socket,
+          context: tls_config,
+          sync_close: true,
+          hostname: domain.name
+        )
+
+        response = yield ssl_layer
+        # TODO log ssl details
+        response
+      else
+        yield tcp_socket
+      end
+    ensure
+      if socket = tcp_socket
+        socket.close
+      end
+    end
+
+    # TODO allow method to be specified
+    def method
+      "GET"
+    end
+
+    def run_request : HTTP::Client::Response
+      with_connected_io do |io|
+        request = HTTP::Request.new method, config.path, headers
+        request.to_io io
+        io.flush
+
+        # decompress: true when Accept-Encoding is set to gzip/deflate
+        HTTP::Client::Response.from_io io#, decompress: true
+      end
+    end
+
+    def config : Monitor::Http
       if (monitor_config = monitor.monitor_config).is_a? Monitor::Http
         monitor_config
       else
@@ -18,14 +83,10 @@ module Monitoring
     end
 
     def check : Nil
-      headers = HTTP::Headers{ "Host" => "#{domain.name}" }
-
       response : HTTP::Client::Response
 
-      client = http_client(host.ip_address)
-
       start_time = Time.monotonic
-      response = client.get config.path, headers
+      response = run_request
       response_time = Time.monotonic - start_time
 
       save_metric "http_response_time", response_time
@@ -40,6 +101,13 @@ module Monitoring
         save_metric "http_status_code", response.status_code, units: "http_status_code"
       end
 
+      # todo scrape:
+      # response.charset
+      # response.content_type
+      # response.mime_type
+      # response.version
+      # response body compression algorithms
+
       if search_string = config.expected_content
         unless response.body.lines.join(" ").index search_string
           log "Plain text search for '#{config.expected_content}' failed", LogEntry.error
@@ -47,9 +115,14 @@ module Monitoring
         end
       end
 
-    rescue e : OpenSSL::SSL::Error
-      log "SSL error"
-      failed!
+    # rescue e : OpenSSL::SSL::Error
+    #   log "SSL error"
+
+    #   if message = e.message
+    #     log message
+    #   end
+
+    #   failed!
 
       # ▸ Job failed! Raised OpenSSL::SSL::Error: SSL_connect: error:14094438:SSL routines:ssl3_read_bytes:tlsv1 alert internal error
 
@@ -71,21 +144,6 @@ module Monitoring
       else
         "http://"
       end
-    end
-
-    def http_client(ip_address : String) : HTTP::Client
-      client = if config.ssl?
-        tls_config = OpenSSL::SSL::Context::Client.new
-        tls_config.verify_mode = OpenSSL::SSL::VerifyMode::NONE
-
-        HTTP::Client.new "#{ip_address}", tls: tls_config
-      else
-        HTTP::Client.new "#{ip_address}"
-      end
-
-      client.connect_timeout = 2
-      client.read_timeout = 2
-      client
     end
   end
 end
